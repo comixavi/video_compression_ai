d4775a42f50ac352e100a0fa6ee9b9051e1f5e7a
Iulian Vornicu <iulian.vornicu@student.tuiasi.ro>
2024-10-03 22:46:56 +0300

    Added visualization of data with probabilities

--- 02_code/training_digits.py
+++ 02_code/training_digits.py
@@ -5,25 +5,28 @@ import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
+# import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-class SimpleNN(nn.Module):
+
+class LinearNN(nn.Module):
     def __init__(self):
-        super(SimpleNN, self).__init__()
+        super(LinearNN, self).__init__()
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(28 * 28, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 10)
+        self.fc2 = nn.Linear(128, 10)
         self.activation = torch.relu
 
     def forward(self, x):
         x = self.flatten(x)
         x = self.activation(self.fc1(x))
-        x = self.fc3(self.fc2(x))
+        x = self.fc2(x)
 
         return x
 
 
-def train(model, loader, criterion, optimizer, epochs=5):
+def train(model, loader, criterion, optimizer, epochs=3):
     model.train()
     for epoch in range(epochs):
         running_loss = 0.0
@@ -35,16 +38,26 @@ def train(model, loader, criterion, optimizer, epochs=5):
             optimizer.step()
 
             running_loss += loss.item()
-        print(f'Epoch [{epoch+1}/{epochs}], Loss: {running_loss/len(loader):.4f}')
+        print(f'Epoch [{epoch + 1}/{epochs}], Loss: {running_loss / len(loader):.4f}')
 
 
-def test(model, loader):
+def test(model, loader, show_figs=False):
     model.eval()
     correct = 0
     total = 0
+
     with torch.no_grad():
         for images, labels in loader:
             outputs = model(images)
+            if show_figs:
+                print(torch.softmax(outputs, dim=1))
+                tfig = torchvision.utils.make_grid(images)
+                tfig = tfig / 2 + 0.5
+                npimg = tfig.numpy()
+                plt.imshow(np.transpose(npimg, (1, 2, 0)))
+                plt.show()
+                input()
+
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
@@ -60,7 +73,7 @@ test_dataset = torchvision.datasets.MNIST(root='./data', train=False, download=T
 train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
 
-model = SimpleNN()
+model = LinearNN()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adagrad(model.parameters(), lr=0.01)
 train(model, train_loader, criterion, optimizer)
